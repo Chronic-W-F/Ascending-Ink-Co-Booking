19c725ce39051d8760b3222b61cac8f89e1fc249
Chronic-W-F <vandergriff4004@comcast.net>
2025-05-08 13:02:45 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import styles from '../styles/Home.module.css'; // make sure this matches exactly
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <h1>Book with Keigan Andrew Ray</h1>
-      <h2>Ascending Ink Co.</h2>
+      <h1 className={styles.heading1}>Book with Keigan Andrew Ray</h1>
+      <h2 className={styles.heading2}>Ascending Ink Co.</h2>
       <a
         href="https://docs.google.com/forms/d/e/1FAIpQLSdCx3szKDqlNAu2vdBOySVDS5dSwUyWsQa8_h_F8n9vCNSxXw/viewform"
         target="_blank"
